69018febd295f226f92c4eb254763e49ab4fa64c
tobiasdam <20456596+tobiasdam@users.noreply.github.com>
2024-09-26 18:43:28 +0200

    fix env var names

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,9 +1,9 @@
 import { env } from '$env/dynamic/public';
 
 export const couchdb = {
-    username: env.COUCHDB_USER ?? "admin",
-    password: env.COUCHDB_PASSWORD ?? "couchdb",
-    host: env.COUCHDB_HOST ?? "localhost",
-    port: env.COUCHDB_PORT ?? 5984,
-    db: env.COUCHDB_DB ?? "mydb"
+    username: env.PUBLIC_COUCHDB_USER ?? "admin",
+    password: env.PUBLIC_COUCHDB_PASSWORD ?? "couchdb",
+    host: env.PUBLIC_COUCHDB_HOST ?? "localhost",
+    port: env.PUBLIC_COUCHDB_PORT ?? 5984,
+    db: env.PUBLIC_COUCHDB_DB ?? "mydb"
 };

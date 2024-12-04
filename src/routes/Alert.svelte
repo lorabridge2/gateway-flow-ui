fb384bec8f8908cd5024f7c8fc7ad6f016b8743f
tobiasdam <20456596+tobiasdam@users.noreply.github.com>
2024-12-04 13:21:59 +0100

    modified alert component for user events

--- src/routes/Alert.svelte
+++ src/routes/Alert.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
 	import { type NodeProps, Handle, Position, useHandleConnections, useStore } from '@xyflow/svelte';
-	import { Hr } from 'flowbite-svelte';
+	import { Hr, Textarea } from 'flowbite-svelte';
 	import FloatInput from './FloatInput.svelte';
 	import { handleClass, saveState } from '$lib/util';
 	import IntegerInput from './IntegerInput.svelte';
+	import TextInput from './TextInput.svelte';
+	import TextAreaInput from './TextAreaInput.svelte';
 
 	interface $$Props extends NodeProps {}
 
-	export let data: $$Props['data'] = { label: 'Node' };
+	export let data: $$Props['data'] = { label: 'Node', message: '' };
 	export let targetPosition: $$Props['targetPosition'] = undefined;
 	export let sourcePosition: $$Props['sourcePosition'] = undefined;
 	// this is a workaround for suppressing the warning about unused props
@@ -19,15 +21,32 @@
 	// $: isConnectable = $connections.length === 0;
 	// const { nodesDraggable, nodesConnectable, elementsSelectable } = useStore();
 	// $: disabled = !($nodesDraggable || $nodesConnectable || $elementsSelectable);
+	let textareaprops = {
+		id: 'message',
+		name: 'message',
+		label: 'Your message',
+		rows: 4,
+		placeholder: 'Leave a comment...'
+	};
 </script>
 
 <div>
-	<Handle type="target" position={targetPosition ?? Position.Top} class={handleClass}/>
+	<Handle type="target" position={targetPosition ?? Position.Top} class={handleClass} />
 	{data?.label}
-	<!-- <Hr hrClass="h-px my-1 bg-gray-200 border-0 dark:bg-gray-700" />
+	<Hr hrClass="h-px my-1 bg-gray-200 border-0 dark:bg-gray-700" />
 	<div class="text-left text-[0.5rem]">
 		<div>Properties:</div>
-		<FloatInput
+		<TextAreaInput class="mt-1" bind:value={data.message} onchange={() => {
+				saveState();
+			}}>Message</TextAreaInput>
+		<!-- <TextInput class="mt-1">Message</TextInput>
+		<Textarea
+			class="!px-2 !py-0 !text-[0.5rem]"
+			style="resize:none;scrollbar-width:thin;"
+			rows="4"
+			placeholder="Your user message"
+		></Textarea> -->
+		<!-- <FloatInput
 			class="mt-1"
 			bind:count={data.min}
 			{disabled}
@@ -42,16 +61,16 @@
 			onchange={() => {
 				saveState();
 			}}>Max Value</FloatInput
-		>
-	</div> -->
-	<Handle type="source" position={sourcePosition ?? Position.Bottom} class={handleClass}/>
+		> -->
+	</div>
+	<Handle type="source" position={sourcePosition ?? Position.Bottom} class={handleClass} />
 </div>
 
 <style>
 	:global(.svelte-flow__node-alert) {
 		padding: 10px;
 		border-radius: 3px;
-		width: 150px;
+		width: 200px;
 		font-size: 0.75rem;
 		color: var(--xy-node-color-default);
 		text-align: center;
---
+++ src/routes/TextAreaInput.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import { ButtonGroup, Label, Input, Textarea } from 'flowbite-svelte';
+	export let value = '';
+	export let filter: Function = undefined;
+	export let placeholder = '';
+	export let disabled = false;
+
+	// if count is directly bound to the input field, every change will be passed to the parent component twice.
+	// The value that was entered and the reactive block calling the filter method.
+	// in this case the bound parent value in e.g. the Hysteresis will try to call saveState twice
+
+	// let val = value;
+	$: {
+		console.log("TextInput");
+		// count = parseInt(count.toString().replace(/[^0-9]*/, ''));
+		if (filter) {
+			value = filter(value) || '';
+		} 
+	}
+</script>
+
+<div {...$$restProps}>
+	<div class="flex">
+		<Label class="mr-2 w-40 break-words text-[0.5rem]"><slot /></Label>
+			<Textarea
+			class="!px-2 !py-0 !text-[0.5rem] h-16 w-32 !leading-[0.75rem]"
+			style="resize:none;scrollbar-width:thin;"
+			rows="4"
+			placeholder="Your user message"
+			bind:value
+		></Textarea>
+			<!-- <Input
+				let:props
+				class="h-3 w-20 px-1 py-0 text-[0.5rem] focus:!border-[#D1D5DB] focus:!ring-0 focus:dark:!border-[#4B5563]"
+			>
+				<input type="text" {...props} bind:value {placeholder} on:change {disabled} />
+			</Input> -->
+	</div>
+</div>

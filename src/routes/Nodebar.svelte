15edc4ee13eb1753c928fc4f4b2be8d68b696ed4
tobiasdam <20456596+tobiasdam@users.noreply.github.com>
2025-01-21 14:28:02 +0100

    added tooltips for nodes

--- src/routes/Nodebar.svelte
+++ src/routes/Nodebar.svelte
@@ -27,37 +27,81 @@
 		<SidebarGroup>
 			<SidebarDropdownWrapper label="Input" isOpen>
 				<div
-					class="input-node node border-2"
+					class="input-node node border-2 relative !pr-7"
 					on:dragstart={(event) => onDragStart(event, 'binarysensor')}
 					draggable={true}
 				>
+					<Tooltip
+						class="!z-100 max-h-40 w-56 overflow-auto bg-white text-black dark:bg-gray-700 dark:text-white text-sm font-[Consolas]"
+						defaultClass="!z-100 py-2 px-3 text-sm font-medium"
+						trigger="click"
+						triggeredBy="#nodebar-sensor"
+						>Sensor node provides sensor data from Zigbee devices.
+						<div class="absolute pointer-events-none block w-[10px] h-[10px] rotate-45 bg-inherit border-inherit" style="left: 98.5px; bottom: -5px;"></div>
+					</Tooltip>
 					<P>Sensor</P>
+					<InfoCircleOutline
+						id="nodebar-sensor"
+						class="absolute right-0 top-0 text-black dark:text-white"
+					/>
 				</div>
 				<div
-					class="default-node node border-2"
+					class="default-node node border-2 relative !pr-7"
 					on:dragstart={(event) => onDragStart(event, 'timer')}
 					draggable={true}
 				>
+					<Tooltip
+						class="max-h-40 w-56 overflow-auto bg-white text-black dark:bg-gray-700 dark:text-white text-sm font-[Consolas]"
+						trigger="click"
+						triggeredBy="#nodebar-timer-switch"
+						>Switches a device on (e.g. light bulb) inside the start/stop time period. Otherwise, a switch off message is sent.
+						<div class="absolute pointer-events-none block w-[10px] h-[10px] rotate-45 bg-inherit border-inherit" style="left: 98.5px; bottom: -5px;"></div>
+					</Tooltip>
 					<P>Timer switch</P>
+					<InfoCircleOutline
+						id="nodebar-timer-switch"
+						class="absolute right-0 top-0 text-black dark:text-white"
+					/>
 				</div>
 			</SidebarDropdownWrapper>
 			<SidebarDropdownWrapper label="Function" isOpen>
 				<div
-					class="default-node node border-2"
+					class="default-node node border-2 relative !pr-7"
 					on:dragstart={(event) => onDragStart(event, 'logicand')}
 					draggable={true}
 				>
+				<Tooltip
+						class="max-h-40 w-56 overflow-auto bg-white text-black dark:bg-gray-700 dark:text-white text-sm font-[Consolas]"
+						trigger="click"
+						triggeredBy="#nodebar-logic-and"
+						>A logical AND is a Boolean operation that returns true only if both conditions are true.
+					</Tooltip>
 					<P>Logic and</P>
+					<InfoCircleOutline
+						id="nodebar-logic-and"
+						class="absolute right-0 top-0 text-black dark:text-white"
+					/>
 				</div>
 				<div
-					class="default-node node border-2"
+					class="default-node node border-2 relative !pr-7"
 					on:dragstart={(event) => onDragStart(event, 'logicor')}
 					draggable={true}
 				>
+					<Tooltip
+						class="max-h-40 w-56 overflow-auto bg-white text-black dark:bg-gray-700 dark:text-white text-sm font-[Consolas]"
+						trigger="click"
+						triggeredBy="#nodebar-logic-or"
+						>
+						A logical OR is a Boolean operation that returns true if at least one condition is true.
+					</Tooltip>
 					<P>Logic or</P>
+					<InfoCircleOutline
+						id="nodebar-logic-or"
+						class="absolute right-0 top-0 text-black dark:text-white"
+					/>
 				</div>
 				<div
-					class="default-node node relative border-2 !pr-7"
+					class="default-node node border-2 relative !pr-7"
 					on:dragstart={(event) => onDragStart(event, 'hysteresis')}
 					draggable={true}
 				>
@@ -65,11 +109,8 @@
 						class="max-h-40 w-56 overflow-auto bg-white text-black dark:bg-gray-700 dark:text-white text-sm font-[Consolas]"
 						trigger="click"
 						triggeredBy="#nodebar-hysteresis"
-						>Hysteresis refers to a delay or lag in a system's response to changes, meaning the
-						current state depends not only on the immediate conditions but also on what happened in
-						the past. For example, in a thermostat, even after reaching the set temperature, the
-						system might take a bit of time before turning off or on again to avoid constantly
-						switching between states.
+						>Hysteresis can be applied to regulate temperature/humidity etc. The node takes a numeric sensor value (e.g. temperature) as input and produces a boolean output to control a device (e.g. a heater). 
+						If a sensor value drops below min value treshold, a "switch on" message is sent towards a control device. Vice versa, a "switch off" message is sent after maximum threshold is reached.
 					</Tooltip>
 					<P>Hysteresis</P>
 					<InfoCircleOutline
@@ -78,34 +119,74 @@
 					/>
 				</div>
 				<div
-					class="default-node node border-2"
+					class="default-node node border-2 relative !pr-7"
 					on:dragstart={(event) => onDragStart(event, 'countdown')}
 					draggable={true}
 				>
+					<Tooltip
+						class="max-h-40 w-56 overflow-auto bg-white text-black dark:bg-gray-700 dark:text-white text-sm font-[Consolas]"
+						trigger="click"
+						triggeredBy="#nodebar-countdown"
+						>Switches a device on (e.g. light bulb) for a period of time based on a binary input (e.g. motion sensor).
+					</Tooltip>
 					<P>Countdown switch</P>
+					<InfoCircleOutline
+						id="nodebar-countdown"
+						class="absolute right-0 top-0 text-black dark:text-white"
+					/>
 				</div>
 				<div
-					class="default-node node border-2"
+					class="default-node node border-2 relative !pr-7"
 					on:dragstart={(event) => onDragStart(event, 'valuefilter')}
 					draggable={true}
 				>
+					<Tooltip
+						class="max-h-40 w-56 overflow-auto bg-white text-black dark:bg-gray-700 dark:text-white text-sm font-[Consolas]"
+						trigger="click"
+						triggeredBy="#nodebar-value-filter"
+						>This node passes data based on a selected comparison operation. Example: If temperature input value is greater than a set value, the input is forwarded to the output. 
+					</Tooltip>
 					<P>Value filter</P>
+					<InfoCircleOutline
+						id="nodebar-value-filter"
+						class="absolute right-0 top-0 text-black dark:text-white"
+					/>
 				</div>
 			</SidebarDropdownWrapper>
 			<SidebarDropdownWrapper label="Output" isOpen>
 				<div
-					class="output-node node border-2"
+					class="output-node node border-2 relative !pr-7"
 					on:dragstart={(event) => onDragStart(event, 'binarydevice')}
 					draggable={true}
 				>
+					<Tooltip
+						class="max-h-40 w-56 overflow-auto bg-white text-black dark:bg-gray-700 dark:text-white text-sm font-[Consolas]"
+						trigger="click"
+						triggeredBy="#nodebar-output"
+						>Output node is a control input to binary Zigbee devices such as light bulbs. 
+					</Tooltip>
 					<P>Output device</P>
+					<InfoCircleOutline
+						id="nodebar-output"
+						class="absolute right-0 top-0 text-black dark:text-white"
+					/>
 				</div>
 				<div
-					class="default-node node border-2"
+					class="default-node node border-2 relative !pr-7"
 					on:dragstart={(event) => onDragStart(event, 'alert')}
 					draggable={true}
 				>
+					<Tooltip
+						class="max-h-40 w-56 overflow-auto bg-white text-black dark:bg-gray-700 dark:text-white text-sm font-[Consolas]"
+						trigger="click"
+						triggeredBy="#nodebar-notification"
+						> A user can set simple string notifications, which are sent as soon as a notification node receives input data.
+					</Tooltip>
 					<P>Notification</P>
+					<InfoCircleOutline
+						id="nodebar-notification"
+						class="absolute right-0 top-0 text-black dark:text-white"
+					/>
 				</div>
 			</SidebarDropdownWrapper>
 		</SidebarGroup>
@@ -141,5 +222,10 @@
 		border-radius: 0.25rem;
 		cursor: grab;
 		width: fit-content;
+	}
+
+	:global(div[role=tooltip]) {
+		z-index: 100;
+		overflow-y:scroll;
 	}
 </style>

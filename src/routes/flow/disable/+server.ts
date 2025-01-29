0bfcfa7abe768f73ae8bb04c6ca42fa7d1cce704
tobiasdam <20456596+tobiasdam@users.noreply.github.com>
2025-01-29 17:26:05 +0100

    debug print

--- src/routes/flow/disable/+server.ts
+++ src/routes/flow/disable/+server.ts
@@ -1,6 +1,7 @@
 import { pushFlowTask } from '$lib/util.server.js';
 
 export async function POST({ params, request }) {
+    console.log("disable called");
     await pushFlowTask({ task: "disable", flow: (await request.json()).payload });
     console.log('flow disabled');
     return new Response(undefined);

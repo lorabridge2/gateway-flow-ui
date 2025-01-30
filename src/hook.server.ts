841de15eb2cdf5f6052ad3321327dc75c8f401f4
tobiasdam <20456596+tobiasdam@users.noreply.github.com>
2025-01-30 15:30:44 +0100

    movede some functions

--- src/hook.server.ts
+++ src/hook.server.ts
@@ -3,9 +3,10 @@ import { env } from '$env/dynamic/private';
 import { redisConfig } from '$lib/config.server';
 // import { Level } from "level";
 import { createClient, TimeSeriesAggregationType, type RedisClientOptions, type RedisClientType } from 'redis';
-import { sseClients, sseMsgsClients } from '$lib/util.server';
+import { sseClients, sseMsgsClients, getDevices, rSep, rEventsPrefix, getSystemMessages, getUserMessages } from '$lib/util.server';
 import { building } from '$app/environment';
 import { json } from '@sveltejs/kit';
+
 // export const db = new Level<string, any>(env.DBPath ?? './db', { valueEncoding: 'json' });
 
 export const client: RedisClientType = createClient({
@@ -16,23 +17,8 @@ export const client: RedisClientType = createClient({
 if (!building) {
     await client.connect();
 }
-const rPrefix = "lorabridge:devman";
-export const rEventsPrefix = "lorabridge:events"
-export const rSep = ":";
-async function getDevices() {
-    if (building) {
-        return [];
-    }
-    let devices: any = {};
-    let lbIDToIeeeMapping = await client.hGetAll([rPrefix, "index:lb"].join(rSep));
-    for (const [key, value] of Object.entries(lbIDToIeeeMapping)) {
-        let name = await client.get([rPrefix, "device:name", key].join(rSep));
-        let attributes = await client.sMembers([rPrefix, "device:attributes", key].join(rSep));
-        devices[key] = { name: name, attributes: attributes, ieee: value, lb_id: key };
-    }
-    return devices;
-};
-export const devices = await getDevices();
+
+export const devices = await getDevices(client);
 
 export const subscriber = client.duplicate()
     .on('error', err => console.error(err));
@@ -41,48 +27,14 @@ export const systemSubscriber = client.duplicate()
 export const userSubscriber = client.duplicate()
     .on('error', err => console.error(err));
 
-async function getMessages(queue: string) {
-    if (building) {
-        return [];
-    }
-    let msgIDs = [];
-    let start = 0;
-    let stop = start + 100 - 1;
-    let res;
-    let msgs = [];
-    console.log([rEventsPrefix, queue, "msgs"].join(rSep));
-    while ((res = await client.zRange([rEventsPrefix, queue, "msgs"].join(rSep), start, stop)).length === 100 || (start === 0 && res.length > 0)) {
-        console.log("length of zRange " + res.length);
-        start = stop + 1;
-        stop = stop + 100;
-        msgIDs = msgIDs.concat(res);
-    }
-
-    for (let index = 0; index < msgIDs.length; index++) {
-        let msg = await client.hGetAll([rEventsPrefix, queue, msgIDs[index]].join(rSep));
-        msg.type = queue;
-        msgs.push(msg);
-    }
-    console.log(msgs);
-    return msgs;
-}
-
-async function getSystemMessages() {
-    return await getMessages("system");
-}
-
-async function getUserMessages() {
-    return await getMessages("user");
-}
-
 console.log("messsages");
-export let systemMessages = await getSystemMessages();
-export let userMessages = await getUserMessages();
-export let mergedMessages = [...systemMessages, ...userMessages].sort((a, b) => parseFloat(b.timestamp) - parseFloat(a.timestamp) )
+export let systemMessages = await getSystemMessages(client);
+export let userMessages = await getUserMessages(client);
+export let mergedMessages = [...systemMessages, ...userMessages].sort((a, b) => parseFloat(b.timestamp) - parseFloat(a.timestamp))
 
 export const reloadMessages = async () => {
-    systemMessages = await getSystemMessages();
-    userMessages = await getUserMessages();
+    systemMessages = await getSystemMessages(client);
+    userMessages = await getUserMessages(client);
     mergedMessages = [...systemMessages, ...userMessages].sort((a, b) => parseFloat(a.timestamp) - parseFloat(b.timestamp))
 };
 
--- src/lib/util.server.ts
+++ src/lib/util.server.ts
@@ -1,5 +1,10 @@
-import { createClient } from 'redis';
+import { createClient, type RedisClientType } from 'redis';
 import { redisConfig } from '$lib/config.server';
+import { building } from '$app/environment';
+
+export const rPrefix = "lorabridge:devman";
+export const rEventsPrefix = "lorabridge:events"
+export const rSep = ":";
 
 export async function pushFlowTask(task) {
     console.log(`redis://${redisConfig.host}:${redisConfig.port}`);
@@ -14,5 +19,54 @@ export async function pushFlowTask(task) {
     await client.quit();
 }
 
-export const sseClients:any = [];
+export async function getDevices(client: RedisClientType) {
+    if (building) {
+        return [];
+    }
+    let devices: any = {};
+    let lbIDToIeeeMapping = await client.hGetAll([rPrefix, "index:lb"].join(rSep));
+    for (const [key, value] of Object.entries(lbIDToIeeeMapping)) {
+        let name = await client.get([rPrefix, "device:name", key].join(rSep));
+        let attributes = await client.sMembers([rPrefix, "device:attributes", key].join(rSep));
+        devices[key] = { name: name, attributes: attributes, ieee: value, lb_id: key };
+    }
+    return devices;
+};
+
+export async function getMessages(queue: string, client: RedisClientType) {
+    if (building) {
+        return [];
+    }
+    let msgIDs = [];
+    let start = 0;
+    let stop = start + 100 - 1;
+    let res;
+    let msgs = [];
+    console.log([rEventsPrefix, queue, "msgs"].join(rSep));
+    while ((res = await client.zRange([rEventsPrefix, queue, "msgs"].join(rSep), start, stop)).length === 100 || (start === 0 && res.length > 0)) {
+        console.log("length of zRange " + res.length);
+        start = stop + 1;
+        stop = stop + 100;
+        msgIDs = msgIDs.concat(res);
+    }
+
+    for (let index = 0; index < msgIDs.length; index++) {
+        let msg = await client.hGetAll([rEventsPrefix, queue, msgIDs[index]].join(rSep));
+        msg.type = queue;
+        msgs.push(msg);
+    }
+    console.log(msgs);
+    return msgs;
+}
+
+export async function getSystemMessages(client: RedisClientType) {
+    return await getMessages("system", client);
+}
+
+export async function getUserMessages(client: RedisClientType) {
+    return await getMessages("user", client);
+}
+
+export const sseClients: any = [];
 export const sseMsgsClients: any = [];
+export const sseStatusClients: any = [];

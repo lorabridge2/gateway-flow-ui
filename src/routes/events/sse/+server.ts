4ef16f406849c6c0d0b64252d6c096b1de37f2ec
tobiasdam <20456596+tobiasdam@users.noreply.github.com>
2025-02-10 11:35:07 +0100

    fix

--- src/routes/events/sse/+server.ts
+++ src/routes/events/sse/+server.ts
@@ -1,12 +1,11 @@
 import { produce } from 'sveltekit-sse'
-import { sseMsgsClients } from '$lib/util.server'
 
 export function POST() {
     return new Response(null, { status: 410 });
     console.log("test");
     return produce(async function start({ emit }) {
         console.log("emit");
-        sseMsgsClients.push(emit);
+        // sseMsgsClients.push(emit);
         console.log("emitted");
     })
 }

7f9cdfe85b99c3579f05682a8b7119209b20ebcd
tobiasdam <20456596+tobiasdam@users.noreply.github.com>
2025-02-10 12:00:01 +0100

    fix var name

--- src/routes/sse/register/flow/+server.ts
+++ src/routes/sse/register/flow/+server.ts
@@ -2,18 +2,19 @@ import { client } from '$hook.server';
 import { mappingFlowToUUID, mappingUUIDToFlow, sseClients } from '$lib/util.server.js';
 
 export async function POST({ params, request }) {
-    let { flow_id, uuid } = await request.json();
+    let { flowId, uuid } = await request.json();
     // if uuid was already registered to a flow
     // remove old association
     if (uuid in mappingUUIDToFlow) {
         console.log("remove old association");
         delete mappingFlowToUUID[mappingUUIDToFlow[uuid]][uuid];
     }
-    mappingUUIDToFlow[uuid] = flow_id;
-    if (!(flow_id in mappingFlowToUUID)) {
-        mappingFlowToUUID[flow_id] = {};
+    mappingUUIDToFlow[uuid] = flowId;
+    if (!(flowId in mappingFlowToUUID)) {
+        mappingFlowToUUID[flowId] = {};
     }
-    mappingFlowToUUID[flow_id][uuid] = sseClients[uuid];
+    mappingFlowToUUID[flowId][uuid] = sseClients[uuid];
+    console.log(mappingFlowToUUID);
     console.log("registered");
     return new Response(null, {
         status: 200
